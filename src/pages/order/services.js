cd06570b187ee8daf71714e53752dfc064546c64
cc <102075776@qq.com>
2019-09-04 16:26:12 +0800

    订单礼拜

--- src/pages/order/services.js
+++ src/pages/order/services.js
@@ -1,11 +1,28 @@
 import axios from 'axios'
 
 // 获取订单种类列表
-export const fetchOrderList = () => axios.post('/appapi.php?c=Merchantapp&a=order_list_all', {
-  ticket: localStorage.getItem('ticket'),
+export const fetchOrderList = () => axios.get('/appapi.php?c=Merchantapp&a=order_list_all', {
+  params: {
+    ticket: localStorage.getItem('ticket'),
+  },
 })
 
 // 获取零售订单列表
-export const fetchShopOrderList = () => axios.post('/appapi.php?c=Merchantapp&a=shop_order', {
-  ticket: localStorage.getItem('ticket'),
+export const fetchShopOrderList = (page, size, status, paytype, searchtype, keyword) => axios.get('/appapi.php?c=Merchantapp&a=shop_order', {
+  params: {
+    page,
+    size,
+    status,
+    pay_type: paytype,
+    searchtype,
+    keyword,
+    ticket: localStorage.getItem('ticket'),
+  },
+})
+
+// 获取零售订单状态
+export const fetchShopOrderStatus = () => axios.get('/appapi.php?c=Merchantapp&a=shop_status_list', {
+  params: {
+    ticket: localStorage.getItem('ticket'),
+  },
 })
--- src/pages/order/store.js
+++ src/pages/order/store.js
@@ -9,6 +9,14 @@ class OrderStore {
   // 零售订单列表
   @observable shopOrderList = []
 
+  @observable shopOrderListPage = 1
+
+  @observable shopOrderListSize = 10
+
+  @observable shopOrderListTotal = null
+
+  @observable shopOrderStatus = []
+
   // 获取订单种类列表
   @action
   fetchOrderList = async () => {
@@ -22,13 +30,65 @@ class OrderStore {
 
   // 获取零售订单列表
   @action
-  fetchShopOrderList = async () => {
-    const response = await services.fetchShopOrderList()
+  fetchShopOrderList = async (status, paytype, searchtype, keyword) => {
+    let hasMore = true
+    if (this.shopOrderListTotal !== null) {
+      hasMore = this.shopOrderListPage * this.shopOrderListSize < this.shopOrderListTotal
+      if (hasMore) {
+        this.shopOrderListPage += 1
+      }
+    }
+    const response = await services.fetchShopOrderList(
+      this.shopOrderListPage,
+      this.shopOrderListSize,
+      status,
+      paytype,
+      searchtype,
+      keyword,
+    )
+    if (response.data.errorCode === ErrorCode.SUCCESS) {
+      if (hasMore) {
+        runInAction(() => {
+          const arr = this.shopOrderList
+          arr.push(...response.data.result.lists)
+          this.shopOrderList = arr
+          this.shopOrderListTotal = response.data.result.total - 0
+        })
+      } else {
+        const remainder = this.shopOrderListTotal % this.shopOrderListSize
+        if (remainder) {
+          runInAction(() => {
+            this.shopOrderList.splice(this.shopOrderListTotal - remainder, remainder)
+            const arr = this.shopOrderList
+            arr.push(...response.data.result.lists)
+            this.shopOrderList = arr
+            this.shopOrderListTotal = response.data.result.total - 0
+          })
+        }
+      }
+    }
+  }
+
+  // 获取零售订单状态
+  @action
+  fetchShopOrderStatus = async () => {
+    const response = await services.fetchShopOrderStatus()
     if (response.data.errorCode === ErrorCode.SUCCESS) {
       runInAction(() => {
-        this.shopOrderList = response.data.result
+        this.shopOrderStatus = response.data.result
       })
     }
+  }
+
+  // 重置页数请求零售列表
+  @action
+  resetAndFetchShopOrderList = async (status, paytype, searchtype, keyword) => {
+    runInAction(() => {
+      this.shopOrderList = []
+      this.shopOrderListPage = 1
+      this.shopOrderListTotal = null
+      this.fetchShopOrderList(status, paytype, searchtype, keyword)
+    })
   }
 }
 
