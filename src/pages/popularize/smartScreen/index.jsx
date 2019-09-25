3dd081298e7c85815d7ae3b4855667e1f821bafb
cc <102075776@qq.com>
2019-09-25 13:28:53 +0800

    智能屏

--- src/pages/popularize/smartScreen/index.jsx
+++ src/pages/popularize/smartScreen/index.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
 import NavBar from '@/common/NavBar'
+import { Icon, Picker } from 'antd-mobile'
+
+const seasons = [
+  [
+    {
+      label: '2013',
+      value: '2013',
+    },
+    {
+      label: '2014',
+      value: '2014',
+    },
+  ],
+  [
+    {
+      label: '春',
+      value: '春',
+    },
+    {
+      label: '夏',
+      value: '夏',
+    },
+  ],
+]
 
 class SmartScreen extends React.Component {
-  componentDidMount() {}
+  state = {
+    open: false,
+    value: ['2013', '夏'],
+  }
 
   render() {
+    const { open, value } = this.state
     return (
       <>
-        <NavBar title="智能屏推广" goBack />
-        123
+        <NavBar
+          title="智能屏推广"
+          goBack
+          right={
+            <Picker data={seasons} title="选择季节" cascade={false} value={value}>
+              <Icon type="ellipsis" onClick={() => this.setState({ open: !open })} />
+            </Picker>
+          }
+        />
       </>
     )
   }
